e5fce5823d14af5c5198f1af39436b5bd9b87f3d
kvns07 <k.venkatnagsai@gmail.com>
2022-04-09 22:26:48 +0530

    init

---
+++ lib/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'rounded_button.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:flutter/material.dart';
+
+//code for designing the UI of our text field where the user writes his email id or password
+
+const kTextFieldDecoration = InputDecoration(
+    hintText: 'Enter a value',
+    hintStyle: TextStyle(color: Colors.black),
+    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ),
+
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.redAccent, width: 2.0),
+      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+    ));
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+final _auth = FirebaseAuth.instance;
+
+class _LoginScreenState extends State<LoginScreen> {
+  String email;
+  String password;
+  bool showSpinner = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                    //Do something with the user input.
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'ENTER YOUR EMAIL',
+                  )),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                    //Do something with the user input.
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'ENTER YOUR PASSWORD.')),
+              SizedBox(
+                height: 50.0,
+              ),
+              RoundedButton(
+                  colour: Colors.redAccent,
+                  title: 'LOG IN',
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.pushNamed(context, 'home_screen');
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
